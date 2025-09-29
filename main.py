268bcc7ebf187eea16b6011bd80cf447b80b8813
urMatriarch <bevgbates@gmail.com>
2025-09-29 13:24:34 -0700

    1.00 Basic Setup Complete

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
+import sys
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
+    try:
+        prompt = sys.argv[1]
+    except:
+        print("Error! No prompt provided! Please provide a prompt a try again!")
+        sys.exit(1)
+
+    additional_args = []
+    try:
+        for i in range(2, len(sys.argv)):
+            additional_args.append(sys.argv[i])
+    except:
+        additional_args = []
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
        model="gemini-2.0-flash-001", 
-       contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+       contents=messages
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if "--verbose" in additional_args:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response: ")
     print(response.text)
 
