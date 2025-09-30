57ededc6df5b7914ae17b28be03f55aae64aad4b
urMatriarch <bevgbates@gmail.com>
2025-09-30 14:32:53 -0700

    1.04: Very solid.

--- tests.py
+++ tests.py
@@ -12,4 +12,6 @@ print(get_file_content("calculator", "pkg/calculator.py"))
 print(get_file_content("calculator", "/bin/cat"))
 print(get_file_content("calculator", "pkg/does_not_exist.py"))
 
-print(write_file("calculator", "beans.txt", "beans"))
+print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+print(write_file("calculator", "/tmp/temp.txt", " this should not be allowed"))
